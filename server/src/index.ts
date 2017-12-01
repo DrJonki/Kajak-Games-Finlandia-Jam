f3c232db567ce33ccdbe5bec13cb20172b90ff6f
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-12-01 20:21:02 +0200

    add error listener

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,11 @@ console.log('Server run(ning) at port', commander.port);
 const server = dgram.createSocket('udp4');
 let jsonString = '';
 
+server.on('error', (err) => {
+  console.log(`server error:\n${err.message}`);
+  server.close();
+});
+
 server.on('listening', () => {
   const address = server.address();
   console.log('UDP Server listening on ' + address.address + ":" + address.port);
@@ -26,20 +31,7 @@ server.on('message', (message, remote) => {
 
 server.bind(commander.port);
 
-const nets = net.createServer((socket) => {
-  socket.end('goodbye\n');
-}).on('error', (err) => {
-  // handle errors here
-  throw err;
-});
-
-// grab an arbitrary unused port.
-nets.listen(9000, () => {
-  console.log('opened server on');
-});
-
 process.on('SIGTERM', () => {
   server.close();
-  nets.close();
   console.log('Closing...');
 });
