dab703df8207149ef77cb9445aed0d29e080a515
Patrick <19pjk85@hotmail.fi>
2017-12-03 14:40:26 +0200

    no message

--- server/src/shoot.ts
+++ server/src/shoot.ts
@@ -5,11 +5,11 @@ import Player from './player'
 export default class Shoot {
     package = 'boomHeadshot'
     playerID
-    croshairPosition
+    crosshairPosition
     time
     constructor(id, croshairPos) {
         this.playerID = id
-        this.croshairPosition = new Vec(croshairPos[0], croshairPos[1])
+        this.crosshairPosition = new Vec(croshairPos[0], croshairPos[1])
         for(let key in g.players) {
             if(this.detectHitWithPlayer(key) && this.detectClearLineOfSight(g.players[key])) {
                 g.players[key].die(g.players[this.playerID])
@@ -21,7 +21,10 @@ export default class Shoot {
     }
     detectHitWithPlayer(id){
         console.log('detecting hit with '+ id)
-        return g.players[id].position.distanceToVector(this.croshairPosition) < 10 ? true : false
+        console.log(g.players[id].position)
+        console.log(this.crosshairPosition)
+        console.log(g.players[id].position.distanceToVector(this.crosshairPosition))
+        return g.players[id].position.distanceToVector(this.crosshairPosition) < 10 ? true : false
     }
 }
 
