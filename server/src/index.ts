9e242186220f461c81aaf6f4f4d3a893eff1b9f9
Patrick <19pjk85@hotmail.fi>
2017-12-03 12:38:39 +0200

    no message

--- server/src/index.ts
+++ server/src/index.ts
@@ -56,15 +56,17 @@ g.server.on('message', function (message, remote) {
                 }, remote.address+':'+remote.port)
 
                 for(let key in g.players) {
-                    g.players[remote.address+':'+remote.port].send(
-                        {
-                            package: 'join', 
-                            data:{ 
-                                id: g.players[key].ip + ':' + g.players[key].port,
-                                side: g.players[key].side
+                    if(key !== remote.address+':'+remote.port) {
+                        g.players[remote.address+':'+remote.port].send(
+                            {
+                                package: 'join', 
+                                data:{ 
+                                    id: g.players[key].ip + ':' + g.players[key].port,
+                                    side: g.players[key].side
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
 
             break
