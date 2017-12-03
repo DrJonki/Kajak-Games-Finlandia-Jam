ae762066de0288cb409a97647ddd758d3b311e80
Patrick <19pjk85@hotmail.fi>
2017-12-03 12:25:45 +0200

    no message

--- server/src/move.ts
+++ server/src/move.ts
@@ -1,6 +1,7 @@
 import g from './global'
 import Vec from './vec'
 import Player from './player'
+import {has} from 'Lodash'
 
 export default class Move {
     package = 'Move'
@@ -10,18 +11,19 @@ export default class Move {
     constructor(id, pos) {
         this.playerID = id
         this.position = new Vec(pos[0], pos[1])
-        this.time = Date.now()
         this.do()
     }
 
     do(){
-        if(this.validate()) {
-            console.log(this.playerID)
-            g.players[this.playerID].position = this.position
-            //g.sendAllExcept()
-            // console.log(g.players[this.playerID].position)
-        } else {
-            this.sendPlayerBackToPreviousPosition()
+        if(has(g.players, this.playerID)) {
+            if(this.validate()) {
+                console.log(this.playerID)
+                g.players[this.playerID].position = this.position
+                //g.sendAllExcept()
+                // console.log(g.players[this.playerID].position)
+            } else {
+                this.sendPlayerBackToPreviousPosition()
+            }
         }
     }
 
