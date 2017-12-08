4749b2b48bfeb09f4a9adb8d50056bab09a3ad85
Patrick <19pjk85@hotmail.fi>
2017-12-08 16:24:02 +0200

    no message

--- server/src/package.ts
+++ server/src/package.ts
@@ -1,21 +1,20 @@
 import g from './global'
 import {includes} from 'lodash'
 class PackageManager {
-    name
-    aply
     packages = {}
     constructor() {
     }
 
     create(name, method) {
-        this.name = name
         if(!includes(this.packages, name)) {
-            this.packages['name'] = method
+            //console.log('creating package')
+            this.packages[name] = method
         }
     }
     apply(obj, remote) {
         try {
-            this.packages[obj.data.name](obj, remote)
+            // console.log('ASDASD >>> ' + typeof obj.data.name)
+            this.packages[obj.package](obj, remote)
         }
         catch(err){console.log(err)}
     }
